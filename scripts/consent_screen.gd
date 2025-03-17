47695f508ea66f5fdbfc235c90de6aa43e31c44c
synthinityofstars <pateldevin07@gmail.com>
2025-03-16 22:06:45 -0500

    0.0.8
    Made checkboxes and check buttons in containers, and slightly kinda optimized, kinda didnt optimize, the checkboxes because they wouldnt work and would just crash otherwise. also doing version naming. X.Y.Z. z is for tiny updates, y is for slightly bigger ones, and X for the ones im proud of.

--- scripts/consent_screen.gd
+++ scripts/consent_screen.gd
@@ -7,6 +7,8 @@ func _on_fullscreen_check_button_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		
+#----------------
 
 # Toggles the Breaking Bad Mexico Filter
 func _on_mexico_filter_check_button_toggled(toggled_on) -> void:
@@ -14,54 +16,53 @@ func _on_mexico_filter_check_button_toggled(toggled_on) -> void:
 		$MexicoFilterRect.show()
 	else:
 		$MexicoFilterRect.hide()
+		
+#----------------- 
 
 #Calling all the buttons and warnings for the required check
-@onready var Check1 = $StroknitCheck
-@onready var Check2 = $DiddlerCheck
-@onready var Check3 = $RightsForfeitCheck
+@onready var Check1 = $OptionsVContainer/Checkboxes/StroknitCheck
+@onready var Check2 = $OptionsVContainer/Checkboxes/DiddlerCheck
+@onready var Check3 = $OptionsVContainer/Checkboxes/RightsForfeitCheck
 @onready var Okbutton = $OkButton
 @onready var WarningLabel = $WarningLabel
 
 #Declares that there is a condition to check all boxes, and it's not fulfilled by default
 var all_checked: bool = false
 
-#Connects signals to the checks about being toggled
+#Connects signals to the checkboxes when the scene is called, seeing if
+#the boxes are checked, and what to do if they are.
+
+
 func _ready() -> void:
-#Is checkbox 1 checked?
-	Check1.toggled.connect(_on_check_box_toggled)
+# Is Check 1, 2, and 3 checked? If so, send the "on check box toggled" signal.
+	for checkbox in [Check1, Check2, Check3]: checkbox.toggled.connect(_on_check_box_toggled)
 
-#Is checkbox 2 checked?
-	Check2.toggled.connect(_on_check_box_toggled)
-
-#Is checkbox 3 checked?
-	Check3.toggled.connect(_on_check_box_toggled)
-
-#Did you press the ok button?
+#Did you press the ok button? if so, send the "on ok button pressed" signal
 	Okbutton.pressed.connect(_on_ok_button_pressed)
 
 #update the button state of condition before we get into it, just in case
 	_update_button_state()
 
+#thus concludes what the ready function does
 
 #if ANY check box is toggled, update the state of the button condition.
 #The signal that actually cares about the checkboxes being toggled is here.
 func _on_check_box_toggled(_toggled_on: bool) -> void:
 	_update_button_state()
 
-#What actually happens when the button condition is updated
+#What actually happens to update the condition of the button, and what the condition is
 func _update_button_state() -> void:
-	
+	all_checked = Check1.button_pressed and Check2.button_pressed and Check3.button_pressed
 	#This bit is confusing so listen up.
 	#these checks aren't given the "toggle" signal. that's because the "toggle" signal
 	#isn't a boolean, it just says "HEY, I'M TOGGLED, DOES ANYONE CARE?" this does not.
 	#the real boolean is "button pressed", which sounds like it isnt a toggle
 	#BUT it has "toggle mode" on, so it actually is.
-	all_checked = Check1.button_pressed and Check2.button_pressed and Check3.button_pressed
-	
 
 #What happens when you press the ok button
 func _on_ok_button_pressed() -> void:
 	#Note that this button DOES NOT have toggle mode on, so it is not treated like a toggle
+	#because of that, it only updates when the user clicks it.
 	if all_checked:
 		WarningLabel.visible = true
 		WarningLabel.text = "ALL SYSTEMS GO!!!"
